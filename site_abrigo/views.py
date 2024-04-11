3476035aff0844cc1dd64f53bf9870952faf813f
Ana Paula Badaró <apbadaro@skiff.com>
2024-04-11 00:35:32 -0300

    Corrige barra_de_pesquisa

--- site_abrigo/views.py
+++ site_abrigo/views.py
@@ -4,11 +4,24 @@ from adocoes.models import Animal
 
 # Create your views here.
 
+
 # Página Principal do Site:
 def site(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def animais_disponiveis(request):
-    bichinho = Animal.objects.all()
-    contexto = contexto = {'bichinho': bichinho,}
-    return render(request, 'animais_disponiveis.html', contexto )
+    query = request.GET.get("q")
+    if query:
+        if query.lower() == "gato":
+            bichinho = Animal.objects.filter(especie__iexact="G")
+        elif query.lower() == "cachorro":
+            bichinho = Animal.objects.filter(especie__iexact="C")
+        else:
+            bichinho = Animal.objects.filter(
+                nome__icontains=query
+            ) | Animal.objects.filter(raca__icontains=query)
+    else:
+        bichinho = Animal.objects.all()
+    contexto = {"bichinho": bichinho}
+    return render(request, "animais_disponiveis.html", contexto)
