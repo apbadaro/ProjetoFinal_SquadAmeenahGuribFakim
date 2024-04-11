f53419e4345f4acad746a5832effa58ade6e0527
Marcia-Moreira <mm.no.linkedin@gmail.com>
2024-04-11 14:38:14 -0300

    Formulario Solicitação de Adoção para Banco de Dados e Front End páginas de Lista e Detalhes Pets.

--- detalhe_pedido/views.py
+++ detalhe_pedido/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from adocoes.models import Animal
 from .forms import AdocaoForm
+from adocoes.models import Solicitante  # Importa a classe Solicitante do app "adocoes"
+# from adocoes.forms import SolicitanteForm
+from .forms import SolicitanteForm
 
 # Create your views here.
 
@@ -10,15 +13,27 @@ def detalhe_pedido(request, animal_id):
     form = AdocaoForm(request.POST or None)
     
     if request.method == 'POST' and form.is_valid():
-        # Lógica para processar o formulário de adoção e enviar para o admin
-        # Exemplo: salvar os dados do formulário no banco de dados ou enviar um e-mail
         # Após processar, redirecione para uma página de sucesso
-        return render(request, 'sucesso.html')
+        return render(request, 'aguarde_retorno.html')
 
     return render(request, 'detalhes_pet_pedido_adocao.html', {'animal': animal, 'form': form})
 
 def sucesso(request):
     return render(request, 'aguarde_retorno.html')
 
+# apagar teste
 def teste_disponiveis(request):
     return render(request, '0_lista_pets.html')
+
+# =========================================================================
+# rever:
+
+def solicitar_adocao(request):
+    if request.method == 'POST':
+        form = SolicitanteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pagina_sucesso')  # redirecionar para a página de sucesso
+    else:
+        form = SolicitanteForm()
+    return render(request, 'solicitante_form.html', {'form': form})
